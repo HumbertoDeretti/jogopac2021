c76ae64caa7a1dabcd2a9f1650150d31b8736374
Jonatha Junkes <59458328+jonathajunkes@users.noreply.github.com>
2021-05-11 20:49:48 -0300

    Init Project

---
+++ LogicGamePAC/src/br/edu/catolicasc/braintech/Inicio.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.edu.catolicasc.braintech;
+
+/**
+ * @author Brain Tech group 3 from PAC
+ *
+ */
+public class Inicio {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Word");
+
+	}
+
+}
