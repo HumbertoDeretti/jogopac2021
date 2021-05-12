53a417678b7f4d2f3ba7593bf7c6aa699474d141
jjunkes <jjunkes@HMJ05050.hmj.local>
2021-05-11 22:25:37 -0300

    Criado Classe de Conexão com Banco de Dados em Mysql

---
+++ JogoPAC_2021/src/br/edu/catolicasc/services/Mysql.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package br.edu.catolicasc.services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * @author jjunkes
+ *
+ */
+public class Mysql {
+	private Connection conn = null;
+	private String user;
+	private String password;
+	private String host;
+	private String port;
+	private String base;
+	
+	
+	/**
+	 * @param user - Usuário do Banco de Dados
+	 * @param password - Senha do Banco de Dados
+	 * @param host - IP ou Domínio do Banco de Dados
+	 * @param port - Porta do Banco de Dados
+	 * @param base - Nome da Banco de Dados
+	 */
+	public Mysql(String user,String password,String host,String port,String base) {
+		this.user = user;
+		this.password = password;
+		this.host = host;
+		this.port = port;
+		this.base = base;
+		Conectar();
+	}
+	private boolean Conectar() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://"+this.host+":"+this.port+"/"+this.base+"",this.user,this.password);
+			return true;
+		} catch (Exception e) {
+			System.out.println(e);
+			return false;
+		}
+	}
+	
+	public boolean Close() {
+		try {
+			conn.close();
+			return true;			
+		} catch (Exception e) {
+			System.out.println(e);
+			return false;
+		}
+		
+	}
+	
+}
