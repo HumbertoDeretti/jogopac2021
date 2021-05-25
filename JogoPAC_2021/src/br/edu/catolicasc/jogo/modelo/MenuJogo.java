8ae9bb4b5569b5deedf126942b0a2dd1c598cf33
Cliente <Cliente@DESKTOP-DDOLVP6>
2021-05-25 20:03:53 -0300

    MenuJogo

---
+++ JogoPAC_2021/src/br/edu/catolicasc/jogo/modelo/MenuJogo.java
@@ -0,0 +1,57 @@
+package br.edu.catolicasc.jogo.modelo;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.text.Normalizer.Form;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+public class MenuJogo extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuJogo frame = new MenuJogo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MenuJogo() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1024, 728);
+		contentPane = new JPanel();
+		contentPane.setLayout(null);
+		setContentPane(contentPane);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon(MenuJogo.class.getResource("/resources/QuadroBranco.png")));
+		lblNewLabel_1.setSize(434, 404);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel = new JLabel();
+		lblNewLabel.setLocation(0, -14);
+		lblNewLabel.setAlignmentY(0.0f);
+		lblNewLabel.setIcon(new ImageIcon(MenuJogo.class.getResource("/resources/SalaDeAula.png")));
+		lblNewLabel.setSize(1008, 718);
+		contentPane.add(lblNewLabel);
+		
+		
+	}
+}
