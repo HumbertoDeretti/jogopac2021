644517bb2e51086b0726bf07d68e8b0f7abf277e
jjunkes <jjunkes@HMJ05050.hmj.local>
2021-06-25 17:35:38 -0300

    UP

--- JogoPAC_2021/src/br/edu/catolicasc/jogo/fase/ButtonRespostas.java
+++ JogoPAC_2021/src/br/edu/catolicasc/jogo/fase/ButtonRespostas.java
@@ -4,15 +4,10 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.border.Border;
-
 import br.edu.catolicasc.parametros.Globais;
 
 public class ButtonRespostas extends JButton implements ActionListener, MouseListener {
--- JogoPAC_2021/src/br/edu/catolicasc/jogo/modelo/CadastroPessoas.java
+++ JogoPAC_2021/src/br/edu/catolicasc/jogo/modelo/CadastroPessoas.java
@@ -26,6 +26,7 @@ public class CadastroPessoas extends JFrame {
 	private static final long serialVersionUID = 983030616361762662L;
 	private JTextField txtNomePessoa;
 	private JTextField txtDataNascimento;
+	@SuppressWarnings("unused")
 	private MysqlUtil bd = MysqlUtil.getInstance();
 	private ComponentesUtils cUtils = new ComponentesUtils();
 	/**
